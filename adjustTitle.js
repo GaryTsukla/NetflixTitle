c86345aaa51770f3930e51f2c92106484af7ac17
garytsukla <gary@tsukla.com>
2022-08-25 00:54:59 +0100

    Increased reliability

--- adjustTitle.js
+++ adjustTitle.js
@@ -1,5 +1,16 @@
 let checkTitle = function() {
 	let h2=document.querySelector('h2');
-	document.title = h2.innerText.slice(0,100);
+	if(!h2){return;}
+	console.log(h2);
+	if(h2.id=='pc-title'){
+		let r=window.localStorage.getItem('show-title');
+		document.title=r;
+		console.log('retrieved title: '+r);
+		return;
+	}
+	let t=h2.innerText.slice(0,100);
+	document.title = t;
+	window.localStorage.setItem('show-title',t);
+	console.log('stored title: '+t);
 }
 setTimeout(checkTitle,5000);
